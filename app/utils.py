96c3d8d8a6e42dd9a82e8fd3c40eff2f52c2c33d
ian293382 <moshou5571228@gmail.com>
2025-05-11 06:59:57 +0000

    docs:Change file name  utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,20 +2,23 @@ import configparser
 import os
 from aioredis import from_url, Redis
 
-# config setting #
-CONFIG_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "app/config.ini"))
+# Config setting
+CONFIG_FILE_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "app/config.ini")
+)
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE_PATH)
 
-# **🔧 Redis setting**
+# Redis settings
 REDIS_HOST = config["redis"]["host"]
 REDIS_PORT = int(config["redis"]["port"])
 REDIS_DB = int(config["redis"]["db"])
 REDIS_ENCODING = config["redis"]["encoding"]
 REDIS_DECODE_RESPONSES = config.getboolean("redis", "decode_responses")
 
-# **🔄 建立連線池**
+# Connect to Redis Pool
 redis_pool = None
+
 
 async def get_redis_client() -> Redis:
     global redis_pool
@@ -25,6 +28,9 @@ async def get_redis_client() -> Redis:
             url,
             db=REDIS_DB,
             encoding=REDIS_ENCODING,
-            decode_responses=REDIS_DECODE_RESPONSES
+            decode_responses=REDIS_DECODE_RESPONSES,
+            max_connections=3,
+            socket_timeout=5,
+            retry_on_timeout=True
         )
     return redis_pool
