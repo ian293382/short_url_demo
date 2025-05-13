f5b52c4ff20ac8f13f520b3383a3e3a6ee6a85ab
ian293382 <moshou5571228@gmail.com>
2025-05-13 15:42:22 +0800

    chore:remove useless comment short_url_demo

--- app/routers/shorter_url.py
+++ app/routers/shorter_url.py
@@ -12,8 +12,7 @@ from ..schemas import ShortURLRequest, ShortURLResponse
 router = APIRouter(tags=["shorter_url"])
 
 
-# 短網址服務設定
-EXPIRATION_SECOND = 60*60*24*30  # 預設過期時間 30 天
+EXPIRATION_SECOND = 60*60*24*30  # default: 30 DAYS
 
 
 def handle_error(exception: Exception):
@@ -33,20 +32,20 @@ def handle_error(exception: Exception):
     response_model=ShortURLResponse,
 )
 async def create_short_url(request: Request, body: ShortURLRequest) -> ShortURLResponse:
-    # 直接限速，不需要傳遞參數
+
     await limit_user_requests(request)
 
     try:
         redis_client = await get_redis_client()
         original_url = body.original_url
         
-        # **檢查是否已存在短網址**
+        # **Inspect origin_url is exist**
         existing_short_id = await redis_client.get(f"url_mapping:{original_url}")
         if existing_short_id:
             short_id = existing_short_id
         else:
             short_id = uuid.uuid4().hex[:8]
-            # **建立雙向對應**
+            # **Create Redis Data to mapping**
             await redis_client.setex(short_id, EXPIRATION_SECOND, original_url)
             await redis_client.setex(f"url_mapping:{original_url}", EXPIRATION_SECOND, short_id)
 
